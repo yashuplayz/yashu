787af93e325b54e6d4b3509b9ceff13167dc64d4
Yashwanth K <167610728+yashuplayz@users.noreply.github.com>
2024-12-23 17:21:12 +0530

    Update AuthImagePattern.jsx

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -1,4 +1,4 @@
-Authimage update : const AuthImagePattern = ({ title, subtitle }) => {
+const AuthImagePattern = ({ title, subtitle }) => {
   return (
     <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
       <div className="max-w-md text-center">
@@ -18,3 +18,5 @@ Authimage update : const AuthImagePattern = ({ title, subtitle }) => {
     </div>
   );
 };
+
+export default AuthImagePattern;
