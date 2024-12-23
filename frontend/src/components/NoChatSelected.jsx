ed6c356ee1642e4f4d795d57598802ee833df9a2
Yashwanth K <167610728+yashuplayz@users.noreply.github.com>
2024-12-23 17:40:13 +0530

    Update NoChatSelected.jsx

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -19,7 +19,7 @@ const NoChatSelected = () => {
         {/* Welcome Text */}
         <h2 className="text-2xl font-bold">Welcome to Chattu!</h2>
         <p className="text-base-content/60">
-          Select a conversation from the sidebar to start chatting
+          Select a conversation from the sidebar to start chatting With New People Around!
         </p>
       </div>
     </div>
