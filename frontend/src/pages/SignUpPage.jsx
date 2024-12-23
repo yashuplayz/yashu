d1c0b2bbf2076924ba6b32dfea1e0d4582fa7a08
Yashwanth K <167610728+yashuplayz@users.noreply.github.com>
2024-12-23 17:54:26 +0530

    Update SignUpPage.jsx

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
-import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
+import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquareDot, User } from "lucide-react";
 import { Link } from "react-router-dom";
 
 import AuthImagePattern from "../components/AuthImagePattern";
@@ -46,7 +46,7 @@ const SignUpPage = () => {
                 className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
               group-hover:bg-primary/20 transition-colors"
               >
-                <MessageSquare className="size-6 text-primary" />
+                <MessageSquareDot className="size-6 text-primary" />
               </div>
               <h1 className="text-2xl font-bold mt-2">Create Account</h1>
               <p className="text-base-content/60">Get started with your free account</p>
@@ -146,7 +146,8 @@ const SignUpPage = () => {
 
       <AuthImagePattern
         title="Join our community"
-        subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
+        subtitle="Connect with Stangers, Anonymous Chat And Share feelings Build Friendship & Love
+        Make Sure Your 18+ , Enjoy a safe and respectful Environment Where Everyone are Welcome!"
       />
     </div>
   );
