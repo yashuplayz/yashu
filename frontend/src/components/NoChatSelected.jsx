fa9bbeb4817b42ac4ee1ea77f15afa1d4f25e1a2
Yashwanth K <167610728+yashuplayz@users.noreply.github.com>
2024-12-23 17:29:46 +0530

    Update NoChatSelected.jsx

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -1,4 +1,4 @@
-import { MessageSquare } from "lucide-react";
+import { MessageSquareDot } from "lucide-react";
 
 const NoChatSelected = () => {
   return (
@@ -11,7 +11,7 @@ const NoChatSelected = () => {
               className="w-16 h-16 rounded-2xl bg-primary/10 flex items-center
              justify-center animate-bounce"
             >
-              <MessageSquare className="w-8 h-8 text-primary " />
+              <MessageSquareDot className="w-8 h-8 text-primary " />
             </div>
           </div>
         </div>
